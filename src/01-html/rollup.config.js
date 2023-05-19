9dec1649d38031730e7ce85ef1ec4c085f33b078
curol <crash@crash.engineer>
2023-05-19 18:39:12 +0000

    🥶

--- src/01-html/rollup.config.js
+++ src/01-html/rollup.config.js
@@ -1,15 +1,21 @@
+// Import .env variables
+import dotenv from 'dotenv';
+dotenv.config({path: './.env.dev'});
+// Import rollup plugins
 import typescript from '@rollup/plugin-typescript' // Typescript - https://github.com/rollup/plugins/tree/master/packages/typescript#options
 import html from '@rollup/plugin-html'
 import image from '@rollup/plugin-image'
 import json from '@rollup/plugin-json'
 import url from '@rollup/plugin-url'
 import postcss from 'rollup-plugin-postcss'
-import strip from '@rollup/plugin-strip'
-import terser from '@rollup/plugin-terser'
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
 
 /**
  * Rollup plugins
  * @see https://github.com/rollup/plugins
+ * @type {import('rollup').RollupOptions.plugins}
  */
 const plugins = [
   typescript({ module: 'esnext' }),
@@ -20,20 +26,29 @@ const plugins = [
   json(),
   url(),
   postcss(),
-  strip(),
-  // terser(),
+  serve({
+    open: true,
+    openPage: '/',
+    host: 'localhost',
+    port: 3000,
+    contentBase: ['./example'],
+}),
+livereload({
+    watch: ['./example'],
+    exts: ['html', 'js', 'css'],
+}),
 ]
 
 /**
  * Rollup configuration
  * @see https://rollupjs.org/guide/en/#configuration-files
- *
+ * @type {import('rollup').RollupOptions}
  */
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'dist/bundle.js', // For single file build
-    // dir: 'dist', // For multiple file chunks build
+    // file: 'dist/bundle.js', // For single file build
+    dir: 'dist', // For multiple file chunks build
     format: 'iife', // Format: https://rollupjs.org/guide/en/#outputformat
   },
   plugins,
